80939c9bc18aeb928d8710d530c6c7e561dd922d
Edwin <dev.edwing@gmail.com>
2021-06-07 12:53:00 -0400

    add

--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,12 @@
 let js = 'amazing';
-if(js === 'amazing')alert('this is practice! ');
+console.log(40 + 8+23-10);
 
-console.log(40+8+23-10);
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
