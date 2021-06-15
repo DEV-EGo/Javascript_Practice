a6bf9fbce016585422b8965e6a0d22c22437bcb2
Edwin <dev.edwing@gmail.com>
2021-06-14 21:03:25 -0400

    AND OR Booleans

--- starter/boolean.js
+++ starter/boolean.js
@@ -5,6 +5,18 @@ not specific to just JS
 "AND" operator 
 "TRUE" when all the variables are TRUE
 "OR" determines if its "TRUE" only if one operator is TRUE
+"NOT" operator acts on only one boolean value
+
+example:
+    age = 16
+    Age is >= 20 (false)
+    Age is <= 30 (true)
+    
+    !A is True
+    A and B false 
+    A OR B True
+    !A AND B True
+    A OR !B false
 
 
 */
