53cf5e9bc7346dcb7e7530b8c1d2cf126fb44dc9
Edwin <dev.edwing@gmail.com>
2021-06-22 19:41:31 -0400

    athtlete function calls

--- fundametals-part=2/functions.js
+++ fundametals-part=2/functions.js
@@ -32,7 +32,8 @@ const Number = (`23`);
 // ===================PRACTICE==============================================
 
 function edwin(age,race){
-    const male = `Hi i am ${age} and im from ${race}`;
+    const male = `Hi i am ${age} 
+        and im from ${race}`;
         return male;
 }
 const EdwinIs = edwin(28,'mexico');
@@ -42,9 +43,39 @@ const ImMexican = edwin(28,"USA");
     console.log(ImMexican);
 // ===================PRACTICE==============================================
 
-function NIKE (shoe,player){
-    const brand = `Michael Jordan ${player} was given the iconic ${shoe}NIKE air shoe`;
+function NIKE (player,shoe){
+    const brand = `${player} 
+        wears ${shoe}`;
         return brand;
 }
-const Athlete = NIKE();
+const Athlete = NIKE("Michael Jordan","SpaceJams");
     console.log(Athlete);
+const LBJ = NIKE ("lebron James" , "South Beaches");
+    console.log(LBJ);
+const Kevin_Durant = NIKE("Kevin Durant", "KD5's");
+    console.log(Kevin_Durant);
+const Kobe = NIKE("Kobe Bryant", "Kobes");
+    console.log(Kobe);
+const Carmelo_Anthony = NIKE("Melo", "team jordans");
+    console.log(Carmelo_Anthony);
+const Ray_allen = NIKE("RAY RAY", "team jordans");
+    console.log(Ray_allen);
+const Zion = NIKE("Zion", "Z tops");
+    console.log(Zion);
+// ===================PRACTICE==============================================
+
+function AlternativeShoes(BBall, kicks){
+        const newBrand = `${BBall} is 
+            signed to ${kicks}`
+            return newBrand;
+}
+
+const Alen_iverson = AlternativeShoes("Alen Iverson", "Reebok");
+    console.log(Alen_iverson);
+
+const La_melo = AlternativeShoes("La melo Ball", "Puma");
+    console.log(La_melo);
+const Ewing = AlternativeShoes("Patrick Ewing", "Reebok");
+    console.log(Ewing);
+const Steph_curry = AlternativeShoes("Steph Curry", "UnderArmor");
+    console.log(Steph_curry);
