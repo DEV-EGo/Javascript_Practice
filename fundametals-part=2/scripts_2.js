1c048b452319274f8619673dba25744d5e1ce3f6
Edwin <dev.edwing@gmail.com>
2021-07-20 13:02:46 -0400

    practice

--- fundametals-part=2/scripts_2.js
+++ fundametals-part=2/scripts_2.js
@@ -1,2 +1,14 @@
 const AverageCalc = (A,B,C) => (A + B + C) /3;
 console.log(AverageCalc(3,4,5));
+
+const DolphinsLiveScore = AverageCalc (44,23,71);
+const KOALASLiveScore = AverageCalc (65,54,49);
+
+console.log(KOALASLiveScore);
+console.log(DolphinsLiveScore);
+
+const WinnersLive = function (KOALASAverageScore, DolphinAveragecore){
+    IF(KOALASAverageScore >= 2 + DolphinAveragecore){
+        console.log(Dolph 🐬(`${DolphinAveragecore} vs ${KOALASAverageScore}`);
+    }
+}
