9a8685e07fbf42f1e3ba698f2bd8d1169d1d8aaf
Edwin <dev.edwing@gmail.com>
2021-07-22 12:50:21 -0400

    array

---
+++ fundametals-part=2/array_practice.js
@@ -0,0 +1,12 @@
+const Friend1 = 'Michael';
+const Friend2 = 'Steven';
+const Friend3 = 'Peter';
+
+const friends= [ 'Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+const years = new Array (1991,1984,2008,2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
