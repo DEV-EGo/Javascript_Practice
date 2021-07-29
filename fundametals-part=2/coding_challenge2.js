46c7a16c0a4cb1752f5ad7e2defce15752f19fea
Edwin <dev.edwing@gmail.com>
2021-07-29 12:50:12 -0400

    finding total for tips

--- fundametals-part=2/coding_challenge2.js
+++ fundametals-part=2/coding_challenge2.js
@@ -5,4 +5,7 @@ const calctip = function (bill) {
 const Bills = [ 125, 555, 44];
 const tips = [calctip(Bills[0]), calctip(Bills[1]), calctip(Bills[2])];
 
-console.log(Bills, tips);
+const totals = [Bills[0] + tips[0],Bills[1] + tips[1],Bills[2] + tips[2]];
+// some of the bills and the tips^^
+
+console.log(Bills, tips, totals);
