a4068ffb4aad45e1822bc5aae24d4046bda93b74
Edwin <dev.edwing@gmail.com>
2021-07-29 12:46:00 -0400

    bills update

--- fundametals-part=2/coding_challenge2.js
+++ fundametals-part=2/coding_challenge2.js
@@ -1,20 +1,8 @@
-const calc_Tip = function(bill){
+const calctip = function (bill) {
     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
 
-// const calc_Tip= bill => bill >= 50 && bill <= 300 ? bill + 0.15 : bill * 0.2; 
-// second way of calling function using arrow method^^
+const Bills = [ 125, 555, 44];
+const tips = [calctip(Bills[0]), calctip(Bills[1]), calctip(Bills[2])];
 
-const bills = [
-    125,
-    555,
-    44
-];
-
-const Tips = [
-    calc_tip(bills[0]),
-    calc_tip(bills[1]),
-    calc_tip(bills[2])
-];
-
-console.log(bills, Tips);
+console.log(Bills, tips);
