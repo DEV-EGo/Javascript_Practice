df62e12129fd89d827f0d3f4b2c1ca14eff58b47
Edwin <dev.edwing@gmail.com>
2021-07-30 12:29:04 -0400

    object js

--- fundametals-part=2/coding_challenge2.js
+++ fundametals-part=2/coding_challenge2.js
@@ -9,3 +9,14 @@ const totals = [Bills[0] + tips[0],Bills[1] + tips[1],Bills[2] + tips[2]];
 // some of the bills and the tips^^
 
 console.log(Bills, tips, totals);
+
+const payingforkicks = function (footwear) {
+    return footwear >= 200 && footwear <= 400 ? footwear * 0.75 : footwear * 0.25;
+}
+
+const payOut = [ 175,220,250];
+const tax = [payingforkicks(footwear[0]), payingforkicks(footwear[1]), payingforkicks(footwear[3])];
+
+const youOwe = [footwear[0] + tax[0], footwear[1] + tax[1], footwear[2] + tax[2]];
+
+console.log(payOut, tax, youOwe);
