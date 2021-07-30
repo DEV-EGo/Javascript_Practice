1ad25ce7f34556f17da18b43a2a6da12314a7493
Edwin <dev.edwing@gmail.com>
2021-07-30 12:37:07 -0400

    object start

---
+++ fundametals-part=2/object.js
@@ -0,0 +1,9 @@
+const jonas = {
+    firstName: 'Jonas',
+    lstName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael','Peter','Steven']
+};
+
+// order of values in objects dont matter when you retrieve them
