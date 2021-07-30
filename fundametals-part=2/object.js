41654826401ad82b0a6ab171b69198b90b80e814
Edwin <dev.edwing@gmail.com>
2021-07-30 12:47:33 -0400

    prompt

--- fundametals-part=2/object.js
+++ fundametals-part=2/object.js
@@ -1,9 +1,23 @@
 const jonas = {
     firstName: 'Jonas',
-    lstName: 'Schmedtmann',
+    lastName: 'Schmedtmann',
     age: 2037 - 1991,
     job: 'teacher',
     friends: ['Michael','Peter','Steven']
 };
+console.log(jonas);
 
 // order of values in objects dont matter when you retrieve them
+
+// dot notation example below
+// 'dot' is an operator that retrieves the property
+console.log(jonas.lastName);
+// can use brackets as a similar way to retrieve property
+console.log(jonas['lastName']);
+
+const name_key = 'Name';
+console.log(jonas['first' + name_key]);
+console.log(jonas['last' + name_key]);
+
+const interested_in = prompt('what do you want to know about jonas? choose between firstName, lastName, age, job, and friends');
+console.log(interested_in)
