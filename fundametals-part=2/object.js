0cfaf32ad6b32c6c0a9d3608f19a7595eedc4dbd
Edwin <dev.edwing@gmail.com>
2021-08-04 12:59:24 -0400

    function methods

--- fundametals-part=2/object.js
+++ fundametals-part=2/object.js
@@ -1,74 +1,99 @@
-const jonas = {
-    firstName: 'Jonas',
-    lastName: 'Schmedtmann',
-    age: 2037 - 1991,
-    job: 'teacher',
-    friends: ['Michael','Peter','Steven']
-};
-console.log(jonas);
+// const jonas = {
+//     firstName: 'Jonas',
+//     lastName: 'Schmedtmann',
+//     age: 2037 - 1991,
+//     job: 'teacher',
+//     friends: ['Michael','Peter','Steven']
+// };
+// console.log(jonas);
 
 // order of values in objects dont matter when you retrieve them
 
 // dot notation example below
 // 'dot' is an operator that retrieves the property
-console.log(jonas.lastName);
+// console.log(jonas.lastName);
 // can use brackets as a similar way to retrieve property
-console.log(jonas['lastName']);
+// console.log(jonas['lastName']);
 
-const name_key = 'Name';
-console.log(jonas['first' + name_key]);
-console.log(jonas['last' + name_key]);
+// const name_key = 'Name';
+// console.log(jonas['first' + name_key]);
+// console.log(jonas['last' + name_key]);
 
-const interested_in = prompt('what do you want to know about jonas? choose between firstName, lastName, age, job, and friends');
-console.log(interested_in)
+// const interested_in = prompt('what do you want to know about jonas? choose between firstName, lastName, age, job, and friends');
+// console.log(interested_in)
 
 // using bracket notation to call 'interested_in' and retrieving data from array
 // accessing property that doest not exist
 
-if(jonas[interested_in]){
-    console.log(jonas[interested_in]);
-}else{
-    console.log('wrong request! what do you want to know about jonas? choose between firstName, lastName, age, job, and friends ')
-}
+// if(jonas[interested_in]){
+//     console.log(jonas[interested_in]);
+// }else{
+//     console.log('wrong request! what do you want to know about jonas? choose between firstName, lastName, age, job, and friends ')
+// }
 
-jonas.location = 'Portugal';
-jonas['twitter'] = '@jonasschmedtman';
-console.log(jonas);
+// jonas.location = 'Portugal';
+// jonas['twitter'] = '@jonasschmedtman';
+// console.log(jonas);
 
 //Challenge request
 // "jonas has 3 friends and his friend is called Michael"
-console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
+// console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
 
 
 // ===================
 
+// console.log('================================================================');
+
+// const Dipset = {
+//     Record_label: 'RocaFella',
+//     City: 'New York',
+//     Members: ['Camron','Jim Jones', 'Juelz Santana', 'Max B']
+// };
+
+// console.log(Dipset);
+
+// console.log(Dipset.Record_label);
+// console.log(Dipset['label']);
+
+// const NYC = (' Harlem music');
+// console.log(NYC);
+
+// const Intro_toMusic = prompt(' HARLEM in the house');
+// console.log(Intro_toMusic);
+
+// if(Dipset[Intro_toMusic]){
+//     console.log(Dipset[Intro_toMusic])
+// }else{
+//     console.log('youre not with the set');
+// }
+
+// Dipset.location = 'Lower East Side';
+// Dipset['Instagram'] = '@Dipster';
+// console.log(Dipset);
+
+// console.log(`${Dipset.Record_label}  signed this music group ${Dipset.Members}`);
+
 console.log('================================================================');
 
-const Dipset = {
-    Record_label: 'RocaFella',
-    City: 'New York',
-    Members: ['Camron','Jim Jones', 'Juelz Santana', 'Max B']
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    birthyear: 1991,
+    job: 'teacher',  //string value
+    friends: ['Michael','Peter','Steven'], //array value
+    hasDriversLicense: true, //boolean value
+
+    // calculateAge: function(birthYear) {
+    //     return 2037 - birthYear;
+    // }
+
+    calculateAge: function(){
+        console.log(this);
+            return 2037 - this.birthyear;
+    }
+    // any function that is attached to an object is called a method^^^
 };
 
-console.log(Dipset);
+console.log(jonas.calculateAge(1991));
 
-console.log(Dipset.Record_label);
-console.log(Dipset['label']);
-
-const NYC = (' Harlem music');
-console.log(NYC);
-
-const Intro_toMusic = prompt(' HARLEM in the house');
-console.log(Intro_toMusic);
-
-if(Dipset[Intro_toMusic]){
-    console.log(Dipset[Intro_toMusic])
-}else{
-    console.log('youre not with the set');
-}
-
-Dipset.location = 'Lower East Side';
-Dipset['Instagram'] = '@Dipster';
-console.log(Dipset);
-
-console.log(`${Dipset.Record_label}  signed this music group ${Dipset.Members}`);
+// console.log(jonas['calculateAge'](1991));
