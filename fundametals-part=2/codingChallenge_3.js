dd5411bc8b8cd73f4c8ccc8fdb8b62423d125773
Edwin <dev.edwing@gmail.com>
2021-08-24 20:16:46 -0400

    update ssh

--- fundametals-part=2/codingChallenge_3.js
+++ fundametals-part=2/codingChallenge_3.js
@@ -1,2 +1,21 @@
-prompt("hey");
-test
+const jonas = {
+    firstname:  'jonas',
+    lastname: 'Schmedtmann',
+    birthYear: 1991,
+    job: 'teacher',
+    friends: ['Michael','Peter','Steven'],
+    hasDriversLicense: true,
+
+    // calculatingAge : function (birthYear) {
+    //     return 2037 - birthYear;
+    // }
+    calculatingAge: function () {
+        console.log(this);
+        return 2037 - this.birthYear;
+    }
+};
+
+
+console.log(jonas.calculatingAge());
+
+// console.log(jonas['calculatingAge'](1991));
