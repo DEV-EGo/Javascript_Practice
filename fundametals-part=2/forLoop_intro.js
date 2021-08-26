b8cf263688a6bb177a3c7f3b1b2a5dbafa527426
Edwin <dev.edwing@gmail.com>
2021-08-26 12:41:53 -0400

    for loop first trial

--- fundametals-part=2/forLoop_intro.js
+++ fundametals-part=2/forLoop_intro.js
@@ -1,5 +1,24 @@
 // for loop will keep running while the condition is TRUE
 
-for(let rep = 1; rep <= 10; rep = rep++) {
+for(let rep = 1; rep <= 30; rep++) {
     console.log("lifing weights starting at 1 set")
 };
+
+const ego = [
+    'Edwin',
+    'Gonzalez',
+    2037-1993,
+    'web developer',
+    ['Noel','Eppy','jordan','juju']
+];
+
+
+// console.log(Edwin[0]);
+// console.log(Edwin[1]);
+// ..
+// console.log(Gonzalez[4])
+
+
+for(let i=0; i<5 ; i++){
+    console.log(ego[i]);
+}
