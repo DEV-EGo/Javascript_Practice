74fe975be1fb44d8483b685ba3e24db4bf5700bd
Edwin <dev.edwing@gmail.com>
2021-09-08 20:14:40 -0400

    for loop practice

---
+++ fundametals-part=2/forloop_practice.js
@@ -0,0 +1,37 @@
+const jonas = [
+    'jonas',
+    'schmedtmann',
+    2037-1991,
+    'teacher',
+    ['Michael', 'peter','steven']
+];
+
+for(let i = jonas.length - 1; i >= 0; i--){
+    console.log(i, jonas[i]);
+}
+
+for(let exercise_test = 1; exercise_test < 4; exercise_test++){
+    console.log(`starting exercise ${exercise_test}`);
+}
+
+// 
+console.log('========    my example  =========');
+// 
+const dog =[
+    'chiquis',
+    'tyson',
+    'duke',
+    2037 - 1993
+];
+
+for (let i =dog.length - 1; i >= 0; i--){
+    console.log(i, dog[i]);
+}
+
+for(let walkDog = 1; walkDog < 5; walkDog++){
+    console.log(`how many times you walked the dog today? ${walkDog}`);
+}
+
+// 
+console.log('========    my example  =========');
+// 
