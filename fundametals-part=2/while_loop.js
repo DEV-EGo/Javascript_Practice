e5df7c44135f707f300413d253a48e8ae8b66c56
Edwin <dev.edwing@gmail.com>
2021-09-10 12:47:49 -0400

    while loop

---
+++ fundametals-part=2/while_loop.js
@@ -0,0 +1,22 @@
+// WHILE LOOP only runs only if the condition is TRUE and
+
+// const rep =[
+//     "lift",
+//     "heavy",
+//     "Run",
+//     "Squat"
+// ];
+
+// for(let rep = 1; rep <= 10; rep++) {
+//     console.log(`lifting ${rep} 💪🏽`);
+// }
+
+// let rep = 1;
+
+// while (rep <= 10){
+//     console.log(` lifting ${rep}`);
+//     rep++;
+// }
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
