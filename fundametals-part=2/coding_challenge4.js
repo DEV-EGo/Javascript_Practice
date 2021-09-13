940717887c5526272f5d7b0d9aa780c37b18b824
Edwin <dev.edwing@gmail.com>
2021-09-12 22:37:10 -0400

    codingChallenge 4

---
+++ fundametals-part=2/coding_challenge4.js
@@ -0,0 +1,25 @@
+// coding challenge 4
+const calc_Tip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+const bills = [
+    22,
+    295,
+    176,
+    440,
+    37,
+    105,
+    10,
+    1100,
+    86,52
+];
+
+const tips = [];
+const totals = [];
+
+for ( let i =0; i < bills.lenght; i++){
+    const tip = calc_Tip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+}
