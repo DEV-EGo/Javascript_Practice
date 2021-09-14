a4b4923d0a1c163d520cde3014a0959603229c52
Edwin <dev.edwing@gmail.com>
2021-09-13 21:07:26 -0400

    new variables

---
+++ fundametals-part=2/coding_challenge4.js
@@ -0,0 +1,19 @@
+const calc_tip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+const bills = [
+    22,
+    295,
+    176,
+    440,
+    37,
+    105,
+    10,
+    1100,
+    86,
+    52
+];
+
+const tips = [];
+const totals = [];
