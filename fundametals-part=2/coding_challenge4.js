e3edb1436a298e7b67ed53ff9804e5a99aabd85f
Edwin <dev.edwing@gmail.com>
2021-09-14 12:24:41 -0400

    coding challenge #4 finished

--- fundametals-part=2/coding_challenge4.js
+++ fundametals-part=2/coding_challenge4.js
@@ -17,3 +17,32 @@ const bills = [
 
 const tips = [];
 const totals = [];
+
+for( let i = 0; i < bills.length; i++){
+    
+    const tip = calc_tip(bills[i]);
+        
+        tips.push(tip);
+        totals.push(tip + bills[i]);
+
+
+}
+// in each iteration a new tip variable is gonna be created
+// simply creates a new one ^^^
+
+console.log(bills, tips,totals);
+
+// ==================bonus============================
+
+const Average_Calc = function(Ar_r){
+
+    let sum = 0;
+    for(let i =0; i < Ar_r.length; i++){
+        // sum = sum + Ar_r[i];
+        sum += Ar_r[i];
+
+    }
+        console.log(sum);
+
+}
+Average_Calc([2,3,6]);
