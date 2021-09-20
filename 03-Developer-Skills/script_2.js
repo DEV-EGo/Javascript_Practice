c4d0c70534977460b24e6c436d6f937dc2ea1fe2
Edwin <dev.edwing@gmail.com>
2021-09-20 12:59:51 -0400

    max min array

--- 03-Developer-Skills/script_2.js
+++ 03-Developer-Skills/script_2.js
@@ -4,3 +4,8 @@ const temperature = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
 // compute the max and min of the temperature?
 // whats a sensor error? & what to do?
+// find min value in temp array
+// find max value in temp _array
+// substract min from max (amplitude) & return it
+
+const calcTempAmplitude = function (temps) {};
