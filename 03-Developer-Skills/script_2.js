76372c88417ef1899bab24456af43bb4dc1c3383
Edwin <dev.edwing@gmail.com>
2021-09-20 20:58:44 -0400

    console log max temp

--- 03-Developer-Skills/script_2.js
+++ 03-Developer-Skills/script_2.js
@@ -8,4 +8,15 @@ const temperature = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 // find max value in temp _array
 // substract min from max (amplitude) & return it
 
-const calcTempAmplitude = function (temps) {};
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    if (temps[i] > max) max = temps[i];
+    if (temps[i] > min) min = temps[i];
+  }
+  console.log(max);
+};
+
+calcTempAmplitude([3, 7, 4]);
