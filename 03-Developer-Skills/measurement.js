8d55fc12053eb9687b86dd2ed755a1ae6ec648d8
Edwin <dev.edwing@gmail.com>
2021-09-22 19:08:01 -0400

    introducing new bug

--- 03-Developer-Skills/measurement.js
+++ 03-Developer-Skills/measurement.js
@@ -5,7 +5,8 @@ const measurementKelvin = function () {
     unit: 'celsius',
 
     // C fix
-    value: Number(prompt('Degrees celsius')),
+    // value: Number(prompt('Degrees celsius')),
+    value: 10,
   };
   //   find the bugg
   console.log(measurement);
@@ -19,3 +20,27 @@ const measurementKelvin = function () {
 };
 
 console.log(measurementKelvin());
+
+// using Debuger
+
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp === 'number') continue;
+
+    if (temps[i] > max) max = temps[i];
+    if (temps[i] < min) min = temps[i];
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+console.log(amplitudeBug);
